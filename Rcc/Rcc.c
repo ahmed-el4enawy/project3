6f71a3201bcd0d400829d23f345664fc00e2e09c
ahmed-el4enawy <ahmed.elshenawy04@eng-st.cu.edu.eg>
2026-04-29 16:48:35 +0300

    sheno

--- Rcc/Rcc.c
+++ Rcc/Rcc.c
@@ -1,9 +1,22 @@
 #include "Rcc.h"
 #include "Rcc_Private.h"
-#include "Bit_Math.h"
 
-void Rcc_EnableGPIOA(void) { SET_BIT(RCC->AHB1ENR, 0); }
-void Rcc_EnableGPIOB(void) { SET_BIT(RCC->AHB1ENR, 1); }
-void Rcc_EnableDMA2(void)  { SET_BIT(RCC->AHB1ENR, 22); }
-void Rcc_EnableTIM3(void)  { SET_BIT(RCC->APB1ENR, 1); }
-void Rcc_EnableADC1(void)  { SET_BIT(RCC->APB2ENR, 8); }
+void Rcc_EnableGPIOA(void) {
+    RCC->AHB1ENR |= (1UL << 0);
+}
+
+void Rcc_EnableGPIOB(void) {
+    RCC->AHB1ENR |= (1UL << 1);
+}
+
+void Rcc_EnableTIM3(void) {
+    RCC->APB1ENR |= (1UL << 1);
+}
+
+void Rcc_EnableADC1(void) {
+    RCC->APB2ENR |= (1UL << 8); /* Enable ADC1 Clock (This was missing!) */
+}
+
+void Rcc_EnableDMA2(void) {
+    RCC->AHB1ENR |= (1UL << 22); /* Enable DMA2 Clock (This was missing!) */
+}
--- Rcc/Rcc.h
+++ Rcc/Rcc.h
@@ -1,12 +1,10 @@
 #ifndef RCC_H
 #define RCC_H
 
-#include "Std_Types.h"
-
 void Rcc_EnableGPIOA(void);
 void Rcc_EnableGPIOB(void);
-void Rcc_EnableDMA2(void);
 void Rcc_EnableTIM3(void);
 void Rcc_EnableADC1(void);
+void Rcc_EnableDMA2(void);
 
 #endif /* RCC_H */
