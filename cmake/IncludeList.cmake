b6f17f9e8a66d2353f08885fa01d660f50472364
ahmed-el4enawy <ahmed.elshenawy04@eng-st.cu.edu.eg>
2026-06-09 18:01:19 +0300

    refactor: Update CMake build system and source lists

--- cmake/IncludeList.cmake
+++ cmake/IncludeList.cmake
@@ -4,15 +4,15 @@ set(INCLUDE_LIST ${INCLUDE_LIST}
         ${PROJECT_PATH}/STM32-base-STM32Cube/CMSIS/ARM/inc
         ${PROJECT_PATH}/STM32-base-STM32Cube/CMSIS/${SERIES_FOLDER}/inc
         ${PROJECT_PATH}/include
-        ${PROJECT_PATH}/Lib
-        ${PROJECT_PATH}/RCC
-        ${PROJECT_PATH}/Gpio
-        ${PROJECT_PATH}/Timer
-        ${PROJECT_PATH}/Pwm
-        ${PROJECT_PATH}/Nvic
         ${PROJECT_PATH}/Adc
-        ${PROJECT_PATH}/Lcd
         ${PROJECT_PATH}/App
+        ${PROJECT_PATH}/Gpio
+        ${PROJECT_PATH}/Lcd
+        ${PROJECT_PATH}/Lib
+        ${PROJECT_PATH}/Nvic
+        ${PROJECT_PATH}/Pwm
+        ${PROJECT_PATH}/Rcc
+        ${PROJECT_PATH}/Timer
 )
 
 if (USE_HAL)
